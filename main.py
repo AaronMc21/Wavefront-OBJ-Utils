fa12cbf444f390f1056f7d831b7b93a78ab85a8c
AaronMc21 <aaron.mcnulty@outlook.com>
2022-12-06 13:11:09 +0000

    feat(obj formatting into c++ formatting)

--- main.py
+++ main.py
@@ -10,13 +10,45 @@ def getOBJs():
             foundOBJs.append(i)
     return foundOBJs
 
-def main():
-    print("Hello OBJ")
-    objs = getOBJs()
-    with open(objs[0]) as f:
+def getComps(objIndex, foundObjs, verts, vertsNormal, faceSets):
+    with open(foundObjs[objIndex]) as f:
         lines = f.readlines()
     for line in lines:
-        print(line)
+        if line.find('v ', 0, 2) != -1:
+            verts.append(line)
+        elif line.find('vn', 0, 2) != -1:
+            vertsNormal.append(line)
+        elif line.find('f ', 0, 2) != -1:
+            faceSets.append(line)
+
+def main():
+    xverts = []
+    xvertsNormal = []
+    xfaceSets = []
+    objs = getOBJs()
+    getComps(0, objs, xverts, xvertsNormal, xfaceSets)
+    for face in xfaceSets:
+        remFace = face.replace('f ', '')
+        remFace2 = remFace.replace('//', ' ')
+        remFace3 = remFace2.replace('\n', '')
+        indexs = remFace3.split(' ')
+        actVerts = []
+        actNormals = []
+        for i in range(len(indexs)):
+            if i%2 == 0:
+                cleanVerts = xverts[int(indexs[i])-1].replace('v ', '')
+                cleanVerts2 = cleanVerts.replace('\n', '')
+                splitVerts = cleanVerts2.split(' ')
+                actVerts.append((splitVerts[0] + 'f, ' + splitVerts[1] + 'f, ' + splitVerts[2] + 'f, '))
+            else:
+                cleanNormals = xvertsNormal[int(indexs[i])-1].replace('vn ', '')
+                cleanNormals2 = cleanNormals.replace('\n', '')
+                splitNormals = cleanNormals2.split(' ')
+                actNormals.append((splitNormals[0] + 'f, ' + splitNormals[1] + 'f, ' +splitNormals[2] + 'f,'))
+
+        print(actVerts[0] + actNormals[0])
+        print(actVerts[1] + actNormals[1])
+        print(actVerts[2] + actNormals[2])
 
 if __name__ == "__main__":
     main()
